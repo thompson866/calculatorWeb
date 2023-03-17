74d6c2eb8678cc43807be2b69049ddaa72500185
arxip <arxipov1989@gmail.com>
2023-03-17 19:17:21 +0300

    web

---
+++ src/main/java/pro/sky/culculatorplus/CulculatorplusApplication.java
@@ -0,0 +1,14 @@
+package pro.sky.culculatorplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulculatorplusApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulculatorplusApplication.class, args);
+	}
+
+}
